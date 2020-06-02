eb0f58bde0eff4b5d4f332d0d77db1d549e87378
dadoris <dadoris@uw.edu>
2020-06-02 02:32:08 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist  = {
+  Slowdive: 'Alison',
+  Phil Ochs: 'Heres to the State of Mississippi' ,
+  My Bloody Valentine: 'Sometimes'  
+};
+
+function  updatePlaylist( obj , artistName, songTitle  ){
+
+  obj[artistName ] = songTitle;
+  return  obj  ;
+}
+
+function updatePlaylist ( obj , artistName, songTitle  ){
+  obj[artistName ] = songTitle;
+  return obj;
+}
